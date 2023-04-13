f92eff0e0b8731425654b21c5f39df2561a6e542
NitrusAphalion <cryptobeardsman@gmail.com>
2023-04-12 23:13:20 -0400

    First commit

---
+++ packages/web/src/lib/Chart.svelte
@@ -0,0 +1,13 @@
+<script>
+    /*
+wasm charts built in rust, rendering with webgl, multithreaded with rayon
+capable of handling any data you send at it, as fast as native desktop application
+example is built with vite and svelte
+    */
+    import init, { render_web_gl } from "vite-wasm-functions";
+    init().then(() => {
+        render_web_gl("canvas");
+    });
+</script>
+
+<canvas id="canvas" width={400} height={400}></canvas>
